bdd04b83cdb846eab975ada7d968f1574ebeb56d
Mauriney <maurineyy@gmail.com>
2022-02-22 18:43:09 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,15 @@
+//Remove acentos das vogais
+function retiraAcentos($texto) {
+    $array1 = array(
+        "á","à","â","ã","ä","é","è","ê","ë","í","ì","î","ï","ó","ò","ô","õ","ö","ú","ù","û","ü","ç","Á","À","Â","Ã","Ä","É","È","Ê","Ë","Í","Ì","Î","Ï","Ó","Ò","Ô","Õ","Ö","Ú","Ù","Û","Ü","Ç"
+    );
+    $array2 = array(
+        "a","a","a","a","a","e","e","e","e","i","i","i","i","o","o","o","o","o","u","u","u","u","c","A","A","A","A","A","E","E","E","E","I","I","I","I","O","O","O","O","O","U","U","U","U","C"
+    );
+    return str_replace($array1, $array2, $texto);
+}
+
+
 //Remove acentos
 function removeAcento(s) {
     const r = s.toLowerCase();
